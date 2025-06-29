05c52b24509a9e41557d06ee01f371561929207a
Md.Mehedi Hasan <129537430+Mehedi-Hasan-soft-web-support@users.noreply.github.com>
2025-06-29 11:02:45 +0600

    Update Esp32_code.ino

--- Esp32_code.ino
+++ Esp32_code.ino
@@ -2,6 +2,7 @@
 #include <HTTPClient.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
+#include <EEPROM.h>
 
 // WiFi credentials
 const char* ssid = "Me";
@@ -28,16 +29,35 @@ byte rowPins[ROWS] = {13, 12, 14, 27};
 byte colPins[COLS] = {26, 25, 33, 32};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
+// EEPROM addresses for vote storage
+#define EEPROM_SIZE 512
+#define ADDR_PRES_VOTE1 0
+#define ADDR_PRES_VOTE2 4
+#define ADDR_SEC_VOTE1 8
+#define ADDR_SEC_VOTE2 12
+#define ADDR_VP_VOTE1 16
+#define ADDR_VP_VOTE2 20
+#define ADDR_COUPON_START 24
+#define ADDR_MAGIC_NUMBER 500  // To check if EEPROM has been initialized
+
 // Vote counters
 int presidentialVotes[2] = {0, 0};
 int secretaryVotes[2] = {0, 0};
 int vpVotes[2] = {0, 0};
 
 // Coupon system
-#define MAX_COUPONS 200
+#define MAX_COUPONS 100
 String couponCodes[MAX_COUPONS] = {
-  "599107", "599108", "599109", "599110", "599111", "599112", "599113", "599114", "599115", "599116", "599117", "599118", "599119", "599120", "599121", "599122", "599123", "599124", "599125", "599126", "599127", "599128", "599129", "599130", "599131", "599132", "599133", "599134", "599135", "599136", "599137", "599138", "599139", "599140", "599141", "599142", "599143", "599144", "599145", "599146", "599147", "599148", "599149", "599150", "599151", "599152", "599153", "599154", "599155", "599156", "599157", "599158", "599159", "599160", "599161", "599162", "599163", "599164", "599165", "599166", "599167", "599168", "599169", "599170", "599171", "599172", "599173", "599174", "599175", "599176", "599177", "599178", "599179", "599180", "599181", "599182", "599183", "599184", "599185", "599186", "599187", "599188", "599189", "599190", "599191", "599192", "599193", "599194", "599195", "599196", "599197", "599198", "599199", "599200",
-
+  "599107", "599108", "599109", "599110", "599111", "599112", "599113", "599114", "599115", "599116", 
+  "599117", "599118", "599119", "599120", "599121", "599122", "599123", "599124", "599125", "599126", 
+  "599127", "599128", "599129", "599130", "599131", "599132", "599133", "599134", "599135", "599136", 
+  "599137", "599138", "599139", "599140", "599141", "599142", "599143", "599144", "599145", "599146", 
+  "599147", "599148", "599149", "599150", "599151", "599152", "599153", "599154", "599155", "599156", 
+  "599157", "599158", "599159", "599160", "599161", "599162", "599163", "599164", "599165", "599166", 
+  "599167", "599168", "599169", "599170", "599171", "599172", "599173", "599174", "599175", "599176", 
+  "599177", "599178", "599179", "599180", "599181", "599182", "599183", "599184", "599185", "599186", 
+  "599187", "599188", "599189", "599190", "599191", "599192", "599193", "599194", "599195", "599196", 
+  "599197", "599198", "599199", "599200", "599201", "599202", "599203", "599204", "599205", "599206"
 };
 bool couponUsed[MAX_COUPONS] = {false};
 
@@ -54,14 +74,80 @@ const unsigned long TIMEOUT = 30000;
 String couponBuffer = "";
 bool validCouponEntered = false;
 
+// EEPROM Functions
+void saveVotesToEEPROM() {
+  EEPROM.put(ADDR_PRES_VOTE1, presidentialVotes[0]);
+  EEPROM.put(ADDR_PRES_VOTE2, presidentialVotes[1]);
+  EEPROM.put(ADDR_SEC_VOTE1, secretaryVotes[0]);
+  EEPROM.put(ADDR_SEC_VOTE2, secretaryVotes[1]);
+  EEPROM.put(ADDR_VP_VOTE1, vpVotes[0]);
+  EEPROM.put(ADDR_VP_VOTE2, vpVotes[1]);
+  EEPROM.commit();
+}
+
+void loadVotesFromEEPROM() {
+  // Check if EEPROM has been initialized
+  int magicNumber;
+  EEPROM.get(ADDR_MAGIC_NUMBER, magicNumber);
+  
+  if (magicNumber != 12345) {
+    // First time initialization
+    presidentialVotes[0] = presidentialVotes[1] = 0;
+    secretaryVotes[0] = secretaryVotes[1] = 0;
+    vpVotes[0] = vpVotes[1] = 0;
+    
+    // Initialize coupon usage
+    for (int i = 0; i < MAX_COUPONS; i++) {
+      couponUsed[i] = false;
+    }
+    
+    // Set magic number and save initial state
+    EEPROM.put(ADDR_MAGIC_NUMBER, 12345);
+    saveVotesToEEPROM();
+    saveCouponStatusToEEPROM();
+  } else {
+    // Load existing data
+    EEPROM.get(ADDR_PRES_VOTE1, presidentialVotes[0]);
+    EEPROM.get(ADDR_PRES_VOTE2, presidentialVotes[1]);
+    EEPROM.get(ADDR_SEC_VOTE1, secretaryVotes[0]);
+    EEPROM.get(ADDR_SEC_VOTE2, secretaryVotes[1]);
+    EEPROM.get(ADDR_VP_VOTE1, vpVotes[0]);
+    EEPROM.get(ADDR_VP_VOTE2, vpVotes[1]);
+    loadCouponStatusFromEEPROM();
+  }
+}
+
+void saveCouponStatusToEEPROM() {
+  for (int i = 0; i < MAX_COUPONS; i++) {
+    EEPROM.put(ADDR_COUPON_START + i, couponUsed[i]);
+  }
+  EEPROM.commit();
+}
+
+void loadCouponStatusFromEEPROM() {
+  for (int i = 0; i < MAX_COUPONS; i++) {
+    EEPROM.get(ADDR_COUPON_START + i, couponUsed[i]);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
+  
+  // Initialize EEPROM
+  EEPROM.begin(EEPROM_SIZE);
+  
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("EVM System Starting");
   lcd.setCursor(0, 1);
-  lcd.print("Please wait...");
+  lcd.print("Loading data...");
+
+  // Load previous vote data
+  loadVotesFromEEPROM();
+  
+  lcd.setCursor(0, 2);
+  lcd.print("Connecting WiFi...");
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -72,6 +158,8 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("WiFi Connected!");
+  lcd.setCursor(0, 1);
+  lcd.print("Data Loaded!");
   delay(2000);
   showWelcomeScreen();
 }
@@ -101,7 +189,16 @@ void handleKeyPress(char key) {
 
     case COUPON_ENTRY:
       if (key == '#') {
-        if (validateCoupon(couponBuffer)) {
+        if (couponBuffer.length() == 0) {
+          // Empty coupon code - show error
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("ERROR: Empty Code");
+          lcd.setCursor(0, 1);
+          lcd.print("Please enter code");
+          delay(2000);
+          showCouponPrompt();
+        } else if (validateCoupon(couponBuffer)) {
           validCouponEntered = true;
           startVoting();
         } else {
@@ -166,6 +263,7 @@ void handleKeyPress(char key) {
     case ADMIN_MODE:
       if (key == '1') showResults();
       else if (key == '2') syncWithCloud();
+      else if (key == '3') resetAllData();
       else if (key == '#') resetVoting();
       break;
   }
@@ -193,9 +291,15 @@ void showCouponPrompt() {
 }
 
 bool validateCoupon(String code) {
+  // Check for empty code first
+  if (code.length() == 0) {
+    return false;
+  }
+  
   for (int i = 0; i < MAX_COUPONS; i++) {
     if (couponCodes[i] == code && !couponUsed[i]) {
       couponUsed[i] = true;
+      saveCouponStatusToEEPROM(); // Save immediately
       return true;
     }
   }
@@ -264,6 +368,10 @@ void castVote() {
   presidentialVotes[selectedCandidate[0]]++;
   secretaryVotes[selectedCandidate[1]]++;
   vpVotes[selectedCandidate[2]]++;
+  
+  // Save to EEPROM immediately
+  saveVotesToEEPROM();
+  
   sendVoteToCloud();
   currentState = VOTE_CAST;
   lcd.clear();
@@ -310,7 +418,7 @@ void enterAdminMode() {
   lcd.setCursor(0, 2);
   lcd.print("2. Sync Cloud");
   lcd.setCursor(0, 3);
-  lcd.print("# to Exit");
+  lcd.print("3. Reset # Exit");
 }
 
 void showResults() {
@@ -330,6 +438,33 @@ void syncWithCloud() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Syncing with Cloud");
+  delay(2000);
+  enterAdminMode();
+}
+
+void resetAllData() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Resetting all data");
+  lcd.setCursor(0, 1);
+  lcd.print("Please wait...");
+  
+  // Reset all vote counts
+  presidentialVotes[0] = presidentialVotes[1] = 0;
+  secretaryVotes[0] = secretaryVotes[1] = 0;
+  vpVotes[0] = vpVotes[1] = 0;
+  
+  // Reset all coupons
+  for (int i = 0; i < MAX_COUPONS; i++) {
+    couponUsed[i] = false;
+  }
+  
+  // Save to EEPROM
+  saveVotesToEEPROM();
+  saveCouponStatusToEEPROM();
+  
+  lcd.setCursor(0, 2);
+  lcd.print("Reset complete!");
   delay(2000);
   enterAdminMode();
 }
